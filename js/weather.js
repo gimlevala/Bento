8fe0840b3f37e38b1df120951bc4011c8227c388
de baro <55126692+gimlevala@users.noreply.github.com>
2021-06-16 03:19:30 +0200

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ var tempUnit = 'C';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87441928fb3cc32d3d72';
+const key = '200910bbb14208ad66a83cf3922bd9fe';
 
 // Set Position function
 setPosition();
